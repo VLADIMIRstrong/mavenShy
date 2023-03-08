ce3a2b6345e3f16330b9165714050b78ee4f8080
Vladimir <vova,erohind82@mail.ru>
2023-03-08 15:11:51 +0300

    src/

--- src/test/java/org/example/Manager/services/MovieTest.java
+++ src/test/java/org/example/Manager/services/MovieTest.java
@@ -1,2 +1,51 @@
+package org.example.Manager.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MovieTest {
+    @Test
+    public void test() {
+        MoviesManager manager = new MoviesManager();
+        manager.add("terminator");
+
+
+        String[] expected = {"terminator" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSecond() {
+        MoviesManager manager = new MoviesManager();
+        manager.add("terminator");
+        manager.add("BloodShot");
+
+
+        String[] expected = {"terminator", "BloodShot" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testThree() {
+        MoviesManager manager = new MoviesManager();
+        manager.add("terminator");
+        manager.add("BloodShot");
+        manager.add("Aliens");
+
+
+        String[] expected = {"terminator", "BloodShot","Aliens" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testZero() {
+        MoviesManager manager = new MoviesManager();
+
+
+
+
+        String[] expected = {  };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
--- src/test/java/org/example/Manager/services/SecondMovieTest.java
+++ src/test/java/org/example/Manager/services/SecondMovieTest.java
@@ -1,2 +1,60 @@
-public class SecondMovieTest {
+package org.example.Manager.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondMovieTest { @Test
+public void test() {
+    MoviesManager manager = new MoviesManager();
+    manager.add("terminator");
+
+
+
+    String[] expected = {"terminator" };
+    String[] actual = manager.findLast();
+    Assertions.assertArrayEquals(expected, actual);
+}
+
+    @Test
+    public void testSecond() {
+        MoviesManager manager = new MoviesManager();
+        manager.add("Terminator");
+        manager.add("BloodShot");
+
+        //  manager.add("ninja");
+        // manager.add("Spawn");
+        //  manager.add("Godzilla");
+        //   manager.add("Rump");
+        //  manager.add("Corn");
+        // manager.add("Apple");
+        // manager.add("Noyce");
+
+
+        String[] expected = {"BloodShot","Terminator" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testSecondManager() {
+        MoviesManager manager = new MoviesManager(11);
+        manager.add("Terminator");
+        manager.add("BloodShot");
+
+        manager.add("Ninja");
+        manager.add("Spawn");
+        manager.add("Godzilla");
+        manager.add("Rump");
+        manager.add("Corn");
+        manager.add("Apple");
+        manager.add("Noyce");
+        manager.add("Blood");
+        manager.add("Blue");
+
+
+
+
+        String[] expected = {"Blue","Blood","Noyce","Apple","Corn","Rump","Godzilla","Spawn","Ninja",
+                "BloodShot","Terminator" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
